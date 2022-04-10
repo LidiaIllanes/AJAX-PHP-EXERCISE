fbafd82d5cd003676bfd71696950f02990467822
lidia illanes <l.illanes.roel@gmail.com>
2022-04-10 13:31:05 +0200

    AJAX PHP exercise

---
+++ index.js
@@ -0,0 +1,27 @@
+// I store the imputs in two variables, the i create another variable i strore the parameters to pass onto the AJAX method,
+let v1 = $('#first_number').val();
+let v2 = $('#second_number').val();
+
+function validate() {
+    if ((v1 === "" || (/\D/.test(v1))) || (v2 === "" || (/\D/.test(v2)))) {
+        alert("What you are trying to sum is not valid, enter a valid number!");
+        return false;
+    } else { return true }
+}
+
+function sum() {
+    if (validate() === true) {
+        let parameters = {
+            'first_number': v1,
+            'second_number': v2
+        }
+        $.ajax({
+            data: parameters,
+            url: 'index.php',
+            type: 'post',
+            success: function (response) {
+                $("#result").html(response);
+            }
+        });
+    }
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<!-- The result contains the result of the sum of the first and second input and then output is displayed -->
+<?php 
+$result = $_POST['first_number'] + $_POST['second_number']; 
+echo $result;
+?>
